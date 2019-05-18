18cc65ef2e7c1c315919e70068005512bc8e03ff
Cyrus <cyrus.frost@hotmail.com>
2019-05-18 09:20:01 +0530

    Fix #46 second instance creation

--- src/main.js
+++ src/main.js
@@ -149,14 +149,22 @@ function createWindow () {
     mainWindowState.manage(win);
 }
 
+var gotTheLock = app.requestSingleInstanceLock();
 
-app.requestSingleInstanceLock();
-app.on('second-instance', () => {
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
     // Someone tried to run a second instance, we should focus our window.
     if (win) {
-        win.show();
+      if (win.isMinimized()){
+        win.restore();
         win.focus();
+      }
+      win.show();
+      win.focus();
     }
-});
+  });
+}
 
 app.on('ready', createWindow);

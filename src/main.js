1f607e0c1433ea89f2ef56a26be23a3e26b97651
Ruslan Bekenev <furyinbox@gmail.com>
2020-04-11 00:03:09 +0300

    Format js code

--- src/main.js
+++ src/main.js
@@ -20,74 +20,74 @@ let contextMenu;
 let config = {};
 
 function handleRedirect(e, url) {
-    // there may be some popups on the same page
-    if(url == win.webContents.getURL()) {
-        return true;
-    }
+  // there may be some popups on the same page
+  if(url == win.webContents.getURL()) {
+    return true;
+  }
 
-    // when user is logged in there is link
-    // asks to update the page. It should be opened
-    // in the app and not in the external browser
-    if (/https:\/\/todoist\.com\/app/.test(url)) {
-        win.reload();
-        return true;
-    }
+  // when user is logged in there is link
+  // asks to update the page. It should be opened
+  // in the app and not in the external browser
+  if (/https:\/\/todoist\.com\/app/.test(url)) {
+    win.reload();
+    return true;
+  }
 
-    /**
-     * In case of google or facebook oauth login
-     * let's create another window and listen for
-     * its "close" event.
-     * As soon as that event fired we can refresh our
-     * main window.
-     */
-    if (/google.+?oauth/.test(url) || /facebook.+?oauth/.test(url)) {
-        e.preventDefault();
-        gOauthWindow = new BrowserWindow();
-        gOauthWindow.loadURL(url);
-        gOauthWindow.on('close', () => {
-            win.reload();
-        })
-        return true;
-    }
+  /**
+   * In case of google or facebook oauth login
+   * let's create another window and listen for
+   * its "close" event.
+   * As soon as that event fired we can refresh our
+   * main window.
+   */
+  if (/google.+?oauth/.test(url) || /facebook.+?oauth/.test(url)) {
+    e.preventDefault();
+    gOauthWindow = new BrowserWindow();
+    gOauthWindow.loadURL(url);
+    gOauthWindow.on('close', () => {
+      win.reload();
+    })
+    return true;
+  }
 
-    e.preventDefault()
-    shell.openExternal(url)
+  e.preventDefault()
+  shell.openExternal(url)
 }
 
 function createTray(win) {
-    tray = new Tray(path.join(__dirname, `icons/${config['tray-icon']}`));
-    contextMenu = Menu.buildFromTemplate([
-      {
-        label: 'Show',
-        click:  function() {
-          win.show();
-        },
-        enabled: false,
-        id: 'show-win'
+  tray = new Tray(path.join(__dirname, `icons/${config['tray-icon']}`));
+  contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show',
+      click:  function() {
+        win.show();
       },
-      {
-        label: 'Hide',
-        click:  function() {
-          win.hide();
-        },
-        id: 'hide-win'
+      enabled: false,
+      id: 'show-win'
+    },
+    {
+      label: 'Hide',
+      click:  function() {
+        win.hide();
       },
-      {
-        label: 'Toggle FullScreen',
-        click:  function() {
-          win.setFullScreen(!win.isFullScreen());
-        },
+      id: 'hide-win'
+    },
+    {
+      label: 'Toggle FullScreen',
+      click:  function() {
+        win.setFullScreen(!win.isFullScreen());
       },
-      {
-        label: 'Quit',
-        click:  function() {
-          app.isQuiting = true;
-          app.quit();
-        }
+    },
+    {
+      label: 'Quit',
+      click:  function() {
+        app.isQuiting = true;
+        app.quit();
       }
-    ]);
-    tray.setToolTip('Todoist');
-    tray.setContextMenu(contextMenu);
+    }
+  ]);
+  tray.setToolTip('Todoist');
+  tray.setContextMenu(contextMenu);
 }
 
 function setCustomUserAgent() {
@@ -101,69 +101,69 @@ function createWindow () {
   setCustomUserAgent();
 
   const configInstance = new ShortcutConfig();
-    config = configInstance.config;
+  config = configInstance.config;
 
-    let mainWindowState = windowStateKeeper({
-      defaultWidth: 800,
-      defaultHeight: 600
-    });
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600
+  });
 
-    // use mainWindowState to restore previous
-    // size/position of window
-    win = new BrowserWindow({
-        x: mainWindowState.x,
-        y: mainWindowState.y,
-        width: mainWindowState.width,
-        height: mainWindowState.height,
-        minHeight: 600,
-        minWidth: 420,
-        title: 'Todoist',
-        icon: path.join(__dirname, 'icons/icon.png')
-    });
+  // use mainWindowState to restore previous
+  // size/position of window
+  win = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    minHeight: 600,
+    minWidth: 420,
+    title: 'Todoist',
+    icon: path.join(__dirname, 'icons/icon.png')
+  });
 
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, (config['beta'] ? 'beta.html' : 'index.html')),
-        protocol: 'file:',
-        slashes: true
-    }));
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, (config['beta'] ? 'beta.html' : 'index.html')),
+    protocol: 'file:',
+    slashes: true
+  }));
 
-    createTray(win);
-    shortcutsInstance = new shortcuts(win, app);
-    shortcutsInstance.registerAllShortcuts();
+  createTray(win);
+  shortcutsInstance = new shortcuts(win, app);
+  shortcutsInstance.registerAllShortcuts();
 
-    // react on close and minimzie
-    win.on('minimize',function(event){
-        event.preventDefault();
-        win.hide();
-    });
+  // react on close and minimzie
+  win.on('minimize',function(event){
+    event.preventDefault();
+    win.hide();
+  });
 
-    win.on('close', function (event) {
-        if(!app.isQuiting){
-            event.preventDefault();
-            win.hide();
-        }
+  win.on('close', function (event) {
+    if(!app.isQuiting){
+      event.preventDefault();
+      win.hide();
+    }
 
-        return false;
-    });
+    return false;
+  });
 
-    win.on('hide', function() {
-        contextMenu.getMenuItemById('show-win').enabled = true;
-        contextMenu.getMenuItemById('hide-win').enabled = false;
-        tray.setContextMenu(contextMenu);
-    });
+  win.on('hide', function() {
+    contextMenu.getMenuItemById('show-win').enabled = true;
+    contextMenu.getMenuItemById('hide-win').enabled = false;
+    tray.setContextMenu(contextMenu);
+  });
 
-    win.on('show', function() {
-        contextMenu.getMenuItemById('show-win').enabled = false;
-        contextMenu.getMenuItemById('hide-win').enabled = true;
-        tray.setContextMenu(contextMenu);
-    });
+  win.on('show', function() {
+    contextMenu.getMenuItemById('show-win').enabled = false;
+    contextMenu.getMenuItemById('hide-win').enabled = true;
+    tray.setContextMenu(contextMenu);
+  });
 
-    win.webContents.on('new-window', handleRedirect)
-    // manage size/positio of the window
-    // so it can be restore next time
-    mainWindowState.manage(win);
+  win.webContents.on('new-window', handleRedirect)
+  // manage size/positio of the window
+  // so it can be restore next time
+  mainWindowState.manage(win);
 }
 
 var gotTheLock = app.requestSingleInstanceLock();
--- src/shortcuts.js
+++ src/shortcuts.js
@@ -2,62 +2,62 @@ const { globalShortcut } = require('electron');
 const {ShortcutConfig} = require('./shortcutConfig');
 
 class shortcuts {
-    constructor(win, app) {
-        this.win = win;
-        this.app = app;
-        this.shortcutConfig = new ShortcutConfig();
-    }
+  constructor(win, app) {
+    this.win = win;
+    this.app = app;
+    this.shortcutConfig = new ShortcutConfig();
+  }
 
-    registerAllShortcuts() {
-        this.registerQuickAddShortcut();
-        this.registerShowHideShortcut();
-        this.registerReloadShortcut();
-        this.registerQuitShortcut();
-    }
+  registerAllShortcuts() {
+    this.registerQuickAddShortcut();
+    this.registerShowHideShortcut();
+    this.registerReloadShortcut();
+    this.registerQuitShortcut();
+  }
 
-    // open quick add popup
-    registerQuickAddShortcut() {
-        globalShortcut.register(this.shortcutConfig.config['quick-add'], () => {
-            this.win.webContents.sendInputEvent({
-                type: "char",
-                keyCode: 'q'
-            });
-            this.win.show();
-            this.win.webContents.send('focus-quick-add');
-        });
-    }
+  // open quick add popup
+  registerQuickAddShortcut() {
+    globalShortcut.register(this.shortcutConfig.config['quick-add'], () => {
+      this.win.webContents.sendInputEvent({
+        type: "char",
+        keyCode: 'q'
+      });
+      this.win.show();
+      this.win.webContents.send('focus-quick-add');
+    });
+  }
 
-    // show/hide
-    registerShowHideShortcut() {
-        globalShortcut.register(this.shortcutConfig.config['show-hide'], () => {
-            if (!this.win.isFocused()) {
-                this.win.show();
-                this.win.focus();
-                return;
-            }
-            
-            this.win.hide();
-        });
-    }
+  // show/hide
+  registerShowHideShortcut() {
+    globalShortcut.register(this.shortcutConfig.config['show-hide'], () => {
+      if (!this.win.isFocused()) {
+        this.win.show();
+        this.win.focus();
+        return;
+      }
 
-    // reload page
-    registerReloadShortcut() {
-        globalShortcut.register(this.shortcutConfig.config['refresh'], () => {
-            if (this.win.isFocused()) {
-                this.win.reload();
-            }
-        });
-    }
+      this.win.hide();
+    });
+  }
 
-    registerQuitShortcut() {
-        globalShortcut.register(this.shortcutConfig.config['quit'], () => {
-            // isQuiting is important for
-            // on('close') event where this variable is checked.
-            // In case it is not true then the app just minimized.
-            this.app.isQuiting = true;
-            this.app.quit();
-        });
-    }
+  // reload page
+  registerReloadShortcut() {
+    globalShortcut.register(this.shortcutConfig.config['refresh'], () => {
+      if (this.win.isFocused()) {
+        this.win.reload();
+      }
+    });
+  }
+
+  registerQuitShortcut() {
+    globalShortcut.register(this.shortcutConfig.config['quit'], () => {
+      // isQuiting is important for
+      // on('close') event where this variable is checked.
+      // In case it is not true then the app just minimized.
+      this.app.isQuiting = true;
+      this.app.quit();
+    });
+  }
 }
 
 module.exports = shortcuts;

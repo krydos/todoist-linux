26d8aa5640979f90b0848a871499bbd28411d841
Ari Birnbaum <ari@ceiphr.com>
2020-09-25 22:36:26 -0400

    Improved context menu.

--- src/main.js
+++ src/main.js
@@ -19,7 +19,25 @@ let tray = null;
 let contextMenu;
 let config = {};
 
+// const CONFIG_FILE_NAME = '.todoist-linux.json'
+
+// function getConfigDirectory() {
+//   if (process.platform == 'win32') {
+//     return process.env.HOMEDRIVE + process.env.HOMEPATH;
+//   }
+
+//   // if possible save config in $XDG_CONFIG_HOME
+//   // which is $HOME/.config by default
+//   if (process.env.XDG_CONFIG_HOME) {
+//     return process.env.XDG_CONFIG_HOME;
+//   }
+
+//   return process.env.HOME + '/.config';
+// }
+
 function createTray(win) {
+  const configInstance = new ShortcutConfig();
+
   // if tray-icon is set to null in config file then don't create a tray icon
   if (!config['tray-icon']) {
     return;
@@ -28,32 +46,108 @@ function createTray(win) {
   tray = new Tray(path.join(__dirname, `icons/${config['tray-icon']}`));
   contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Show',
+      label: 'Open Todoist',
       click:  function() {
         win.show();
       },
-      enabled: false,
       id: 'show-win'
     },
     {
-      label: 'Hide',
-      click:  function() {
-        win.hide();
-      },
-      id: 'hide-win'
+      type: 'separator'
     },
     {
-      label: 'Toggle FullScreen',
+      label: 'Add task',
       click:  function() {
-        win.setFullScreen(!win.isFullScreen());
+        win.webContents.sendInputEvent({
+          type: "keyDown",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "keyUp",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "char",
+          keyCode: 'q'
+        });
+        win.show();
+        win.webContents.send('focus-quick-add');
       },
+      id: 'add-task'
     },
     {
-      label: 'Quit',
+      label: 'Search',
+      click:  function() {
+        win.webContents.sendInputEvent({
+          type: "keyDown",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "keyUp",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "char",
+          keyCode: 'f'
+        });
+        win.show();
+      },
+      id: 'search'
+    },
+    {
+      label: 'Inbox',
+      click:  function() {
+        win.show();
+        win.webContents.send('go-to-anchor', 'filter_inbox');
+      },
+      id: 'inbox'
+    },
+    {
+      label: 'Today',
+      click:  function() {
+        win.show();
+        win.webContents.send('go-to-anchor', 'filter_today');
+      },
+      id: 'today'
+    },
+    {
+      label: 'Upcoming',
+      click:  function() {
+        win.show();
+        win.webContents.send('go-to-anchor', 'filter_upcoming');
+      },
+      id: 'upcoming'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Preferences',
+      click:  function() {
+        shell.openItem(path.join(
+          configInstance.getConfigDirectory(),
+          '.todoist-linux.json'
+        ));
+      },
+      id: 'preferences'
+    },
+    {
+      label: 'Report an issue',
+      click:  function() {
+        shell.openExternal('https://github.com/KryDos/todoist-linux/issues/new');
+      },
+      id: 'report-issue'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit Todoist',
       click:  function() {
         app.forceQuit = true;
         app.quit();
-      }
+      },
+      id: 'quit'
     }
   ]);
   tray.setToolTip('Todoist');
@@ -121,19 +215,6 @@ function createWindow () {
     win.hide();
   });
 
-  win.on('hide', function() {
-    contextMenu.getMenuItemById('show-win').enabled = true;
-    contextMenu.getMenuItemById('hide-win').enabled = false;
-    tray.setContextMenu(contextMenu);
-  });
-
-  win.on('show', function() {
-    contextMenu.getMenuItemById('show-win').enabled = false;
-    contextMenu.getMenuItemById('hide-win').enabled = true;
-    tray.setContextMenu(contextMenu);
-  });
-
-  win.webContents.on('new-window', handleRedirect)
   // manage size/position of the window
   // so it can be restored next time
   mainWindowState.manage(win);

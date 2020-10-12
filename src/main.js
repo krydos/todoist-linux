0afe90066429b3407f2855c5b6e35984f0400889
Ari Birnbaum <ari@ceiphr.com>
2020-10-11 20:38:12 -0400

    Refactored tray

--- src/main.js
+++ src/main.js
@@ -11,132 +11,13 @@ const path = require('path');
 const url = require('url');
 
 const {ShortcutConfig} = require('./shortcutConfig');
+const createTray = require('./tray');
 const shortcuts = require('./shortcuts');
 
 let win = {};
 let gOauthWindow = undefined;
-let tray = null;
-let contextMenu;
 let config = {};
 
-function createTray() {
-  const configInstance = new ShortcutConfig();
-
-  // if tray-icon is set to null in config file then don't create a tray icon
-  if (!config['tray-icon']) {
-    return;
-  }
-
-  tray = new Tray(path.join(__dirname, `icons/${config['tray-icon']}`));
-  contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Open Todoist',
-      click:  function() {
-        win.show();
-      },
-      id: 'show-win'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Add task',
-      click:  function() {
-        win.webContents.sendInputEvent({
-          type: "keyDown",
-          keyCode: "Escape"
-        });
-        win.webContents.sendInputEvent({
-          type: "keyUp",
-          keyCode: "Escape"
-        });
-        win.webContents.sendInputEvent({
-          type: "char",
-          keyCode: 'q'
-        });
-        win.show();
-        win.webContents.send('focus-quick-add');
-      },
-      id: 'add-task'
-    },
-    {
-      label: 'Search',
-      click:  function() {
-        win.webContents.sendInputEvent({
-          type: "keyDown",
-          keyCode: "Escape"
-        });
-        win.webContents.sendInputEvent({
-          type: "keyUp",
-          keyCode: "Escape"
-        });
-        win.webContents.sendInputEvent({
-          type: "char",
-          keyCode: 'f'
-        });
-        win.show();
-      },
-      id: 'search'
-    },
-    {
-      label: 'Inbox',
-      click:  function() {
-        win.show();
-        win.webContents.send('go-to-anchor', 'filter_inbox');
-      },
-      id: 'inbox'
-    },
-    {
-      label: 'Today',
-      click:  function() {
-        win.show();
-        win.webContents.send('go-to-anchor', 'filter_today');
-      },
-      id: 'today'
-    },
-    {
-      label: 'Upcoming',
-      click:  function() {
-        win.show();
-        win.webContents.send('go-to-anchor', 'filter_upcoming');
-      },
-      id: 'upcoming'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Preferences',
-      click:  function() {
-        shell.openItem(path.join(
-          configInstance.getConfigDirectory(),
-          '.todoist-linux.json'
-        ));
-      },
-      id: 'preferences'
-    },
-    {
-      label: 'Report an issue',
-      click:  function() {
-        shell.openExternal('https://github.com/KryDos/todoist-linux/issues/new');
-      },
-      id: 'report-issue'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Quit Todoist',
-      click:  function() {
-        app.forceQuit = true;
-        app.quit();
-      },
-      id: 'quit'
-    }
-  ]);
-  tray.setToolTip('Todoist');
-  tray.setContextMenu(contextMenu);
-}
 
 function setCustomUserAgent() {
   session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
@@ -366,7 +247,7 @@ if (!gotTheLock) {
 
 app.on('ready', () => {
   createWindow();
-  createTray();
+  createTray(config);
 
   win.webContents.on('dom-ready', () => {
     if (config['beta']) {

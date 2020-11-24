7f34a2bb54c9e0b9574db682a197856f0d27766b
Ari <ari@ceiphr.com>
2020-11-23 21:26:50 -0500

    Revert "Refactored main.js"

--- src/main.js
+++ src/main.js
@@ -1,109 +1,377 @@
-const { app, BrowserWindow, Menu } = require("electron");
-const windowStateKeeper = require("electron-window-state");
-const path = require("path");
-const url = require("url");
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+  session
+} = require('electron');
+const windowStateKeeper = require('electron-window-state');
+const shell = require('electron').shell;
+const path = require('path');
+const url = require('url');
 
-const { ShortcutConfig } = require("./shortcutConfig");
-const createTray = require("./tray");
-const createMenuBar = require("./menuBar");
-const shortcuts = require("./shortcuts");
-const util = require("./util");
+const {ShortcutConfig} = require('./shortcutConfig');
+const shortcuts = require('./shortcuts');
 
 let win = {};
+let gOauthWindow = undefined;
+let tray = null;
+let contextMenu;
 let config = {};
 
-function createWindow() {
-    util.setCustomUserAgent();
+function createTray() {
+  const configInstance = new ShortcutConfig();
 
-    const configInstance = new ShortcutConfig();
-    config = configInstance.config;
+  // if tray-icon is set to null in config file then don't create a tray icon
+  if (!config['tray-icon']) {
+    return;
+  }
 
-    let mainWindowState = windowStateKeeper({
-        defaultWidth: 800,
-        defaultHeight: 600,
-    });
+  tray = new Tray(path.join(__dirname, `icons/${config['tray-icon']}`));
+  contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open Todoist',
+      click:  function() {
+        win.show();
+      },
+      id: 'show-win'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Add task',
+      click:  function() {
+        win.webContents.sendInputEvent({
+          type: "keyDown",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "keyUp",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "char",
+          keyCode: 'q'
+        });
+        win.show();
+        win.webContents.send('focus-quick-add');
+      },
+      id: 'add-task'
+    },
+    {
+      label: 'Search',
+      click:  function() {
+        win.webContents.sendInputEvent({
+          type: "keyDown",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "keyUp",
+          keyCode: "Escape"
+        });
+        win.webContents.sendInputEvent({
+          type: "char",
+          keyCode: 'f'
+        });
+        win.show();
+      },
+      id: 'search'
+    },
+    {
+      label: 'Inbox',
+      click:  function() {
+        win.show();
+        win.webContents.send('go-to-anchor', 'filter_inbox');
+      },
+      id: 'inbox'
+    },
+    {
+      label: 'Today',
+      click:  function() {
+        win.show();
+        win.webContents.send('go-to-anchor', 'filter_today');
+      },
+      id: 'today'
+    },
+    {
+      label: 'Upcoming',
+      click:  function() {
+        win.show();
+        win.webContents.send('go-to-anchor', 'filter_upcoming');
+      },
+      id: 'upcoming'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Preferences',
+      click:  function() {
+        shell.openItem(path.join(
+          configInstance.getConfigDirectory(),
+          '.todoist-linux.json'
+        ));
+      },
+      id: 'preferences'
+    },
+    {
+      label: 'Report an issue',
+      click:  function() {
+        shell.openExternal('https://github.com/KryDos/todoist-linux/issues/new');
+      },
+      id: 'report-issue'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit Todoist',
+      click:  function() {
+        app.forceQuit = true;
+        app.quit();
+      },
+      id: 'quit'
+    }
+  ]);
+  tray.setToolTip('Todoist');
+  tray.setContextMenu(contextMenu);
+}
 
-    // use mainWindowState to restore previous
-    // size/position of window
-    win = new BrowserWindow({
-        x: mainWindowState.x,
-        y: mainWindowState.y,
-        width: mainWindowState.width,
-        height: mainWindowState.height,
-        minHeight: 600,
-        minWidth: 450,
-        title: "Todoist",
-        icon: path.join(__dirname, "icons/icon.png"),
-        autoHideMenuBar: true,
-    });
+function setCustomUserAgent() {
+  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+    details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36';
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
+  });
+}
 
-    win.webContents.setVisualZoomLevelLimits(1, 5);
-    Menu.setApplicationMenu(createMenuBar(config, win));
+function createWindow () {
+  setCustomUserAgent();
 
-    // and load the index.html of the app.
-    win.loadURL(
-        url.format({
-            pathname: path.join(__dirname, "index.html"),
-            protocol: "file:",
-            slashes: true,
-        })
-    );
+  const configInstance = new ShortcutConfig();
+  config = configInstance.config;
 
-    shortcutsInstance = new shortcuts(win, app);
-    shortcutsInstance.registerAllShortcuts();
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600
+  });
 
-    // Only send to tray on minimize if user is running with tray and minimizing to tray is allowed
-    win.on("minimize", function (event) {
-        if (config["tray-icon"] && config["minimize-to-tray"]) {
-            event.preventDefault();
-            win.hide();
-        }
-    });
+  // use mainWindowState to restore previous
+  // size/position of window
+  win = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    minHeight: 600,
+    minWidth: 450,
+    title: 'Todoist',
+    icon: path.join(__dirname, 'icons/icon.png'),
+    autoHideMenuBar: true
+  });
 
-    win.on("close", function (event) {
-        if (app.forceQuit || !config["tray-icon"] || !config["close-to-tray"]) {
-            // Do the default electron behaviour which is to close the main window
-            // In production build the app is not closed probably due to this bug - https://github.com/electron/electron/issues/10156
-            // call the app.quit() once again, it helps
+  win.webContents.setVisualZoomLevelLimits(1, 5);
+
+  var menuBar = Menu.buildFromTemplate([
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Preferences',
+          click:  function() {
+            shell.openItem(path.join(
+              configInstance.getConfigDirectory(),
+              '.todoist-linux.json'
+            ));
+          },
+        },
+        {
+          label:'Quit',
+          click:  function() {
             app.forceQuit = true;
             app.quit();
-            return;
-        }
+          },
+          accelerator: config['quit']
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label:'Zoom In',
+          role: 'zoomin',
+          accelerator: 'CommandOrControl+='
+        },
+        {
+          label:'Zoom Out',
+          role: 'zoomout',
+          accelerator: 'CommandOrControl+-'
+        },
+        {
+          label:'Reset Zoom',
+          role: 'resetzoom',
+          accelerator: 'CommandOrControl+0'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label:'Show/Hide',
+          click:  function() {
+            win.hide();
+          },
+          accelerator: config['show-hide']
+        },
+        {
+          label:'Refresh',
+          click:  function() {
+            win.reload();
+          },
+          accelerator: config['refresh']
+        },
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'GitHub',
+          click:  function() {
+            shell.openExternal('https://github.com/KryDos/todoist-linux');
+          },
+        },
+        {
+          label: 'Changelog',
+          click:  function() {
+            shell.openExternal('https://github.com/krydos/todoist-linux/releases');
+          },
+        },
+        {
+          label: 'Report an issue',
+          click:  function() {
+            shell.openExternal('https://github.com/KryDos/todoist-linux/issues/new');
+          },
+        },
+      ]
+    },
+  ])
 
-        event.preventDefault();
-        win.hide();
-    });
+  Menu.setApplicationMenu(menuBar);
 
-    // manage size/position of the window
-    // so it can be restored next time
-    mainWindowState.manage(win);
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  shortcutsInstance = new shortcuts(win, app);
+  shortcutsInstance.registerAllShortcuts();
+
+  // Only send to tray on minimize if user is running with tray and minimizing to tray is allowed
+  win.on('minimize',function(event) {
+    if (config['tray-icon'] && config['minimize-to-tray']) {
+      event.preventDefault();
+      win.hide();
+    }
+  });
+
+  win.on('close', function (event) {
+    if (app.forceQuit || !config['tray-icon'] || !config['close-to-tray']) {
+      // Do the default electron behaviour which is to close the main window
+      // In production build the app is not closed probably due to this bug - https://github.com/electron/electron/issues/10156
+      // call the app.quit() once again, it helps
+      app.forceQuit = true;
+      app.quit();
+      return;
+    }
+
+    event.preventDefault();
+    win.hide();
+  });
+
+  // manage size/position of the window
+  // so it can be restored next time
+  mainWindowState.manage(win);
+}
+
+function handleRedirect(e, url) {
+  // there may be some popups on the same page
+  if (url == win.webContents.getURL()) {
+    return true;
+  }
+
+  // when user is logged in there is link
+  // asks to update the page. It should be opened
+  // in the app and not in the external browser
+  if (/https:\/\/todoist\.com\/app/.test(url)) {
+    win.reload();
+    return true;
+  }
+
+  /**
+   * In case of google or facebook oauth login
+   * let's create another window and listen for
+   * its "close" event.
+   * As soon as that event fired we can refresh our
+   * main window.
+   */
+  if (/google.+?oauth/.test(url) || /facebook.+?oauth/.test(url)) {
+    e.preventDefault();
+    gOauthWindow = new BrowserWindow();
+    gOauthWindow.loadURL(url);
+    gOauthWindow.on('close', () => {
+      win.reload();
+    })
+    return true;
+  }
+
+  /*
+   * The first time the settings button is clicked
+   * the 'new-window' event is emitted with the url to the settings page
+   * The electron default behavior(creating a new window) is prevented
+   * and instead the contents of the main window are reloaded with the contents
+   * from the settings page effectively emulating the behavior of the website
+   */
+  if (/prefs\/account/.test(url)) {
+    e.preventDefault();
+    win.loadURL(url);
+    return true;
+  }
+
+  e.preventDefault()
+  shell.openExternal(url)
 }
 
 var gotTheLock = app.requestSingleInstanceLock();
 
 if (!gotTheLock) {
-    app.quit();
+  app.quit();
 } else {
-    app.on("second-instance", () => {
-        // Someone tried to run a second instance, we should focus our window.
-        if (win) {
-            if (win.isMinimized()) {
-                win.restore();
-                win.focus();
-            }
-            win.show();
-            win.focus();
-        }
-    });
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (win) {
+      if (win.isMinimized()){
+        win.restore();
+        win.focus();
+      }
+      win.show();
+      win.focus();
+    }
+  });
 }
 
-app.on("ready", () => {
-    createWindow();
-    createTray(config, win);
+// app.on('ready', createWindow);
 
-    win.webContents.on("dom-ready", () => {
-        if (config["beta"]) {
-            win.webContents.send("is-beta");
-        }
-    });
-});
+app.on('ready', () => {
+  createWindow();
+  createTray();
+
+  win.webContents.on('dom-ready', () => {
+    if (config['beta']) {
+      win.webContents.send('is-beta');
+    }
+  })
+})
+
--- src/shortcutConfig.js
+++ src/shortcutConfig.js
@@ -1,98 +1,104 @@
-const CONFIG_FILE_NAME = ".todoist-linux.json";
+const CONFIG_FILE_NAME = '.todoist-linux.json'
 
-const { dialog } = require("electron");
-const fs = require("fs");
-const path = require("path");
-const { URL } = require("url");
+const {dialog} = require('electron')
+const fs = require('fs');
+const path = require('path');
+const { URL } = require('url');
 
 class ShortcutConfig {
-    constructor() {
-        this.config = {};
+  constructor() {
+    this.config = {};
 
-        if (this.checkIfConfigFileExists()) {
-            this.updateShortcutsFromConfigFile();
-        } else {
-            this.createDefaultConfigFile();
-            this.updateShortcutsFromConfigFile();
-        }
-
-        this.mergeConfigWithDefaults();
+    if (this.checkIfConfigFileExists()) {
+      this.updateShortcutsFromConfigFile();
+    } else {
+      this.createDefaultConfigFile();
+      this.updateShortcutsFromConfigFile();
     }
 
-    // Make sure all shortcuts are defined.
-    // In case some of them missed we will apply the default one
-    mergeConfigWithDefaults() {
-        this.config = Object.assign(this.getDefaultConfig(), this.config);
+    this.mergeConfigWithDefaults();
+  }
+
+  // Make sure all shortcuts are defined.
+  // In case some of them missed we will apply the default one
+  mergeConfigWithDefaults() {
+    this.config = Object.assign(this.getDefaultConfig(), this.config);
+  }
+
+  updateShortcutsFromConfigFile() {
+    const configPath = path.join(
+      this.getConfigDirectory(),
+      CONFIG_FILE_NAME
+    );
+
+    try {
+      this.config = JSON.parse(
+        fs.readFileSync(
+          new URL('file://' + configPath),
+          'utf8'
+        )
+      );
+    } catch (e) {
+      dialog.showMessageBox({
+        title: 'Config error',
+        message: 'There is error in configuration file. Please make sure it has proper JSON. Default shortcuts will be used.'
+      });
+      this.config = this.getDefaultConfig();
+    }
+  }
+
+  createDefaultConfigFile() {
+    const configPath = path.join(
+      this.getConfigDirectory(),
+      CONFIG_FILE_NAME
+    );
+
+    fs.writeFileSync(
+      configPath,
+      JSON.stringify(
+        this.getDefaultConfig(),
+        null,
+        4
+      )
+    );
+  }
+
+  getConfigDirectory() {
+    if (process.platform == 'win32') {
+      return process.env.HOMEDRIVE + process.env.HOMEPATH;
     }
 
-    updateShortcutsFromConfigFile() {
-        const configPath = path.join(
-            this.getConfigDirectory(),
-            CONFIG_FILE_NAME
-        );
-
-        try {
-            this.config = JSON.parse(
-                fs.readFileSync(new URL("file://" + configPath), "utf8")
-            );
-        } catch (e) {
-            dialog.showMessageBox({
-                title: "Config error",
-                message:
-                    "There is error in configuration file. Please make sure it has proper JSON. Default shortcuts will be used.",
-            });
-            this.config = this.getDefaultConfig();
-        }
+    // if possible save config in $XDG_CONFIG_HOME
+    // which is $HOME/.config by default
+    if (process.env.XDG_CONFIG_HOME) {
+      return process.env.XDG_CONFIG_HOME;
     }
 
-    createDefaultConfigFile() {
-        const configPath = path.join(
-            this.getConfigDirectory(),
-            CONFIG_FILE_NAME
-        );
+    return process.env.HOME + '/.config';
+  }
 
-        fs.writeFileSync(
-            configPath,
-            JSON.stringify(this.getDefaultConfig(), null, 4)
-        );
+  checkIfConfigFileExists() {
+    const configPath = path.join(
+      this.getConfigDirectory(),
+      CONFIG_FILE_NAME
+    );
+    return fs.existsSync(configPath);
+  }
+
+  getDefaultConfig() {
+    return {
+      'quick-add': 'CommandOrControl+Alt+a',
+      'show-hide': 'CommandOrControl+Alt+Q',
+      'refresh': 'CommandOrControl+Alt+r',
+      'beta': false,
+      'quit': 'Alt+F4',
+      'tray-icon': 'icon.png',
+      'minimize-to-tray': true,
+      'close-to-tray': true,
     }
-
-    getConfigDirectory() {
-        if (process.platform == "win32") {
-            return process.env.HOMEDRIVE + process.env.HOMEPATH;
-        }
-
-        // if possible save config in $XDG_CONFIG_HOME
-        // which is $HOME/.config by default
-        if (process.env.XDG_CONFIG_HOME) {
-            return process.env.XDG_CONFIG_HOME;
-        }
-
-        return process.env.HOME + "/.config";
-    }
-
-    checkIfConfigFileExists() {
-        const configPath = path.join(
-            this.getConfigDirectory(),
-            CONFIG_FILE_NAME
-        );
-        return fs.existsSync(configPath);
-    }
-
-    getDefaultConfig() {
-        return {
-            "quick-add": "CommandOrControl+Alt+a",
-            "show-hide": "CommandOrControl+Alt+Q",
-            refresh: "CommandOrControl+Alt+r",
-            beta: false,
-            quit: "Alt+F4",
-            "tray-icon": "icon.png",
-            "minimize-to-tray": true,
-            "close-to-tray": true,
-        };
-    }
+  }
 }
 
 module.exports = {
-    ShortcutConfig,
+  ShortcutConfig
 };
